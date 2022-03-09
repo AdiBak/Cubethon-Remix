915b0220664df1d813db26dbf98ce7bcd14b717b
AdiBak <adityabakshi107@gmail.com>
2022-03-09 15:57:10 -0800

    Committing updates

---
+++ Assets/Scripts/FallCheck.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+ using System.Collections;
+ 
+ [RequireComponent(typeof(Rigidbody))] 
+ public class FallCheck : MonoBehaviour {
+ 
+     public Transform player; 
+     public float maxDistance = 10f; 
+     private Rigidbody rigidbody;
+
+     public bool useGravity = true;
+     void Awake () {
+         rigidbody = GetComponent<Rigidbody> ();
+         //rigidbody.useGravity = false;
+     }
+ 
+     void FixedUpdate(){
+         rigidbody.useGravity = false;
+         if (Vector3.Distance (player.position, transform.position) < maxDistance && useGravity) { //transform is the object that this script is attached to
+             rigidbody.AddForce(Physics.gravity * (rigidbody.mass * rigidbody.mass));
+
+            // rigidbody.WakeUp();
+         }
+     }
+ }
---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform player;	// A variable that stores a reference to our Player
+	public Vector3 offset;		// A variable that allows us to offset the position (x, y, z)
+	
+	// Update is called once per frame
+	void Update () {
+		if (SceneManager.GetActiveScene().buildIndex <= 4){
+		// Set our position to the players position and offset it
+			transform.position = player.position + offset;
+		} 
+	}
+}
